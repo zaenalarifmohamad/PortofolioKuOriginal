3bd98a418e237ec2bf7e588ffcecf534ca57517e
BeBew Go <southernhighindo@gmail.com.com>
2024-02-01 11:27:16 +0700

    two commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const menu = document.querySelector('.menu');
+const barMenu = document.querySelector('.bar-menu');
+const iconBars = document.querySelector('.icon-bars');
+const iconClose = document.querySelector('.icon-close');
+
+barMenu.addEventListener('click', displayMenu);
+menu.addEventListener('click', displayMenu);
+
+function displayMenu(){
+    if(menu.classList.contains('tampil')) {
+       menu.classList.remove('tampil');
+       iconBars.style.display='inline';
+       iconClose.style.display='none';
+    } else {
+       menu.classList.add('tampil');
+       iconBars.style.display='none';
+       iconClose.style.display='inline';
+    }
+}
+
+//LINK KIRIM DI KONTAK ME LEWAT GOOGLESHEET//
+document.addEventListener("DOMContentLoaded", function() {
+   document.getElementById("contact-form").addEventListener("submit", function(event) {
+       event.preventDefault(); // Prevent default form submission
+
+       // Kirim data formulir dengan AJAX
+       var formData = new FormData(this);
+
+       fetch(this.action, {
+           method: this.method,
+           body: formData,
+           headers: {
+               'Accept': 'application/json'
+           }
+       })
+       .then(response => {
+           if (response.ok) {
+               return response.json();
+           }
+           throw new Error('Network response was not ok.');
+       })
+       .then(data => {
+           alert("Pesan berhasil dikirim!");
+           document.getElementById("contact-form").reset(); // Reset formulir
+       })
+       .catch(error => {
+           console.error('There has been a problem with your fetch operation:', error);
+           alert("Pesan gagal dikirim. Silakan coba lagi.");
+       });
+   });
+});
