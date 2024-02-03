dfdf60ef2ea09eab0d04dfa15755917a6acd7711
Arifin Zaenal Mohamad <155322271+zaenalarifmohamad@users.noreply.github.com>
2024-02-03 17:44:58 +0700

    main.js

--- js/main.js
+++ js/main.js
@@ -18,35 +18,51 @@ function displayMenu(){
     }
 }
 
-//LINK KIRIM DI KONTAK ME LEWAT GOOGLESHEET//
 document.addEventListener("DOMContentLoaded", function() {
-   document.getElementById("contact-form").addEventListener("submit", function(event) {
-       event.preventDefault(); // Prevent default form submission
+    document.getElementById("contact-form").addEventListener("submit", function(event) {
+        event.preventDefault(); // Prevent default form submission
 
-       // Kirim data formulir dengan AJAX
-       var formData = new FormData(this);
+        // Kirim data formulir dengan AJAX
+        var formData = new FormData(this);
 
-       fetch(this.action, {
-           method: this.method,
-           body: formData,
-           headers: {
-               'Accept': 'application/json'
-           }
-       })
-       .then(response => {
-           if (response.ok) {
-               return response.json();
-           }
-           throw new Error('Network response was not ok.');
-       })
-       .then(data => {
-           alert("Pesan berhasil dikirim!");
-           document.getElementById("contact-form").reset(); // Reset formulir
-       })
-       .catch(error => {
-           console.error('There has been a problem with your fetch operation:', error);
-           alert("Pesan gagal dikirim. Silakan coba lagi.");
-       });
-   });
+        fetch(this.action, {
+            method: this.method,
+            body: formData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Network response was not ok.');
+        })
+        .then(data => {
+            // Tampilkan toast sukses
+            showToast();
+
+            // Reset formulir
+            document.getElementById("contact-form").reset();
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+            alert("Pesan gagal dikirim. Silakan coba lagi.");
+        });
+    });
+
+    // Fungsi untuk menampilkan toast sukses
+    function showToast() {
+        // Dapatkan elemen toast
+        var toast = document.getElementById('toast');
+
+        // Tampilkan toast dengan menambahkan kelas show
+        toast.classList.add('show');
+
+        // Sembunyikan toast setelah 3 detik
+        setTimeout(function() {
+            toast.classList.remove('show');
+        }, 3000);
+    }
 });
 
