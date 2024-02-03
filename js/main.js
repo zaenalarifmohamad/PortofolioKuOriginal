d93ab5b9a87016267495623f23567db04186a10c
Arifin Zaenal Mohamad <155322271+zaenalarifmohamad@users.noreply.github.com>
2024-02-03 16:06:56 +0700

    main.js

--- js/main.js
+++ js/main.js
@@ -18,19 +18,35 @@ function displayMenu(){
     }
 }
 
+//LINK KIRIM DI KONTAK ME LEWAT GOOGLESHEET//
+document.addEventListener("DOMContentLoaded", function() {
+   document.getElementById("contact-form").addEventListener("submit", function(event) {
+       event.preventDefault(); // Prevent default form submission
 
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
+       // Kirim data formulir dengan AJAX
+       var formData = new FormData(this);
 
-    // Simulate data submission to server (you should replace this with actual server submission)
-    setTimeout(function() {
-        // Show toast
-        var toast = document.getElementById('toast');
-        toast.classList.add('show');
-
-        // Hide toast after 3 seconds
-        setTimeout(function() {
-            toast.classList.remove('show');
-        }, 3000);
-    }, 1000); // Simulated delay to mimic server request
+       fetch(this.action, {
+           method: this.method,
+           body: formData,
+           headers: {
+               'Accept': 'application/json'
+           }
+       })
+       .then(response => {
+           if (response.ok) {
+               return response.json();
+           }
+           throw new Error('Network response was not ok.');
+       })
+       .then(data => {
+           alert("Pesan berhasil dikirim!");
+           document.getElementById("contact-form").reset(); // Reset formulir
+       })
+       .catch(error => {
+           console.error('There has been a problem with your fetch operation:', error);
+           alert("Pesan gagal dikirim. Silakan coba lagi.");
+       });
+   });
 });
+
